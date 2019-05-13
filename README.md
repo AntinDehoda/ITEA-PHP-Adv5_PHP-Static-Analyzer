1395f0a3df81e4c619cd783ee66ed307f7f71ca8
AntinDehoda <dehoda@ukr.net>
2019-05-13 10:44:48 +0300

    Adds Exeption & info

--- README.md
+++ README.md
@@ -14,6 +14,9 @@ Usage
 `$ ./bin/console stat:class-author <email> <project-src>` - counts classes/interfaces/trait
 created by some developer in project.
 
+`./bin/console stat:class-signature Greeflas\\StaticAnalyzer\\Analyzer\\ClassSignatureCounter` - Shows type, 
+counts methods and properties of specified class.
+
 Code style fixer
 ----------------
 
---
+++ src/Exeptions/InvalidClassNameExeption.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the "PHP Static Analyzer" project.
+ *
+ * (c) Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Greeflas\StaticAnalyzer\Exceptions;
+
+/**
+ * Class InvalidClassNameException
+ *
+ * @author Anton Degoda <dehoda@ukr.net>
+ */
+class InvalidClassNameExeption extends \DomainException
+{
+}
